b0d8528c9f7d1e6451b7b5390ffb3062feacc3f9
Luke Thompson <endeavour9@gmail.com>
2022-07-14 13:25:22 +1000

    fix the values

--- main.cpp
+++ main.cpp
@@ -6,28 +6,29 @@ using namespace surd;
 using namespace std;
 
 vector<Surd> calculateSurds(unsigned radicand) {
-  return {Surd{
-              .coefficient = 1,
-              .radicand = radicand,
-          },
-          Surd{.coefficient = 2,
-           .radicand = 153,
-           .workingStep = WorkingStep{.squareNumber = 4, .divisibilityRule = surd::DivisibilityRule::FOUR, .previousRadicand = 612}
-          },
-          Surd{.coefficient = 6,
-           .radicand = 17,
-           .workingStep = WorkingStep{.squareNumber = 9, .divisibilityRule = surd::DivisibilityRule::NINE, .previousRadicand = 153}
-          }
-  };
+    return {Surd{
+            .coefficient = 1,
+            .radicand = radicand,
+    },
+            Surd{.coefficient = 2,
+                    .radicand = 153,
+                    .workingStep = WorkingStep{.squareNumber = 4, .divisibilityRule = surd::DivisibilityRule::FOUR, .previousRadicand = 612}
+            },
+            Surd{.coefficient = 6,
+                    .radicand = 17,
+                    .workingStep = WorkingStep{.squareNumber = 9, .divisibilityRule = surd::DivisibilityRule::NINE, .previousRadicand = 153}
+            }
+    };
 }
 
 int main() {
-  vector<Surd> surds = calculateSurds(612);
-  cout << "Calculating the simplified surd for the sqrt(" << 612 << ")" << endl;
-  for (const auto &surd : surds) {
-      if (surd.workingStep) {
-          cout << surd << endl;
-      }
-  }
-  return 0;
+    int radicand = 612;
+    vector<Surd> surds = calculateSurds(radicand);
+    cout << "Calculating the simplified surd for the sqrt(" << radicand << ")" << endl;
+    for (const auto &surd: surds) {
+        if (surd.workingStep) {
+            cout << surd << endl;
+        }
+    }
+    return 0;
 }
